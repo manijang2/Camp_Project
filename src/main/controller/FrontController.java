2b615e6a867f4dd7c018abce258da1231768bf8e
manijang2 <manijang2@naver.com>
2018-03-29 00:17:18 +0900

    DAO / DTO 및 나머지 inc 파일, 자바파일 설정

---
+++ src/main/controller/FrontController.java
@@ -0,0 +1,46 @@
+package main.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		ActionForward forward = null;
+		Action action = null;
+
+		System.out.println("command : " + command);
+
+		if (command.equals("/main.do")) {
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("/main/main.jsp");
+		}
+
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+}

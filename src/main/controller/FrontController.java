98cc7da2adb10b299031551d60514f6d140c29a7
no_word <zxcvbnm78912@gmail.com>
2018-04-04 15:48:28 +0900

    front controller에 loginAction 추가

--- src/main/controller/FrontController.java
+++ src/main/controller/FrontController.java
@@ -73,6 +73,21 @@ public class FrontController extends javax.servlet.http.HttpServlet implements j
 			forward.setPath("/admin/admin_main.jsp");
 
 		}
+		
+		/*------------------Member controller Start------------------*/
+		else if(command.equals("/member/MemberLoginAction.do")) {
+			action = new MemberLoginAction();
+			try {
+				forward = action.execute(request, response);
+				System.out.println("forward 성공");
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		}
+		
+		/*------------------Member controller End------------------*/
+
 
 		if (forward != null) {
 			if (forward.isRedirect()) {
